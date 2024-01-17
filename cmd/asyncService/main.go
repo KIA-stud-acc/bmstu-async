a2c3cd31cf3170f6ed7c0208d4fc13f57e68b40d
Илья <iliakekin52@gmail.com>
2024-01-17 22:07:34 +0300

    first commit

--- cmd/asyncService/main.go
+++ cmd/asyncService/main.go
@@ -12,13 +12,13 @@ type PK struct {
 
 func main() {
 	r := gin.Default()
-	r.POST("/set_status", func(c *gin.Context) {
+	r.POST("/calculate_votes", func(c *gin.Context) {
 		var request PK
 		if err := c.BindJSON(&request); err != nil {
 			// DO SOMETHING WITH THE ERROR
 		}
 		// Отправка PUT-запроса к основному серверу
-		url := "http://127.0.0.1:8000/applications/putQuantityOfVotes/" // Замените на ваш реальный URL
+		url := "http://127.0.0.1:8000/applications/putQuantityOfVotes" // Замените на ваш реальный URL
 		go api.SendStatus(request.PK, url)
 		c.JSON(http.StatusOK, gin.H{"message": "Status update initiated"})
 	})
